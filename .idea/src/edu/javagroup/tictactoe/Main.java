47a64ee75848492d8e0352b2760b324c2ccca535
sppreu-cmyk <sppreu@icloud.com>
2026-04-24 17:33:46 +0300

    1

---
+++ .idea/src/edu/javagroup/tictactoe/Main.java
@@ -0,0 +1,10 @@
+package edu.javagroup.tictactoe;
+
+import edu.javagroup.tictactoe.frame.MainFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new MainFrame().setVisible(true);
+    }
+}
