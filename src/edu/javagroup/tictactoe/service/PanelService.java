7e3c8e980908e31c499dc78675644448b1f67219
sppreu-cmyk <sppreu@icloud.com>
2026-04-26 14:34:13 +0300

    tssk3 complit

--- src/edu/javagroup/tictactoe/service/PanelService.java
+++ src/edu/javagroup/tictactoe/service/PanelService.java
@@ -50,12 +50,52 @@ public record PanelService(Panel panel) {
     }
 
     public int checkByVertical(int numberVertical, char symbol) {
-        
+        char[][] grid = panel.getPanels();
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            if (grid[i][numberVertical] == (symbol)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int checkByHorizontal(int numberHorizontal, char symbol) {
+        char[][] grid = panel.getPanels();
+        int count = 0;
+        for (int i = 0; i < grid[0].length; i++) {
+            if (grid[numberHorizontal][i] == (symbol)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int checkByDiagonalLin(char symbol) {
+        char[][] grid = panel.getPanels();
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            if (grid[i][grid.length - 1 - i] == (symbol)) {
+                count++;
+            }
+        }
+        return count;
 
     }
 
 
-}
+    public int checkByDiagonalWin(char symbol) {
+        char[][] grid = panel.getPanels();
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            if (grid[i][i] == (symbol)) {
+                count++;
+            }
+        }
+            return count;
+
+        }
+    }
 
 
 
